1c2eb8e963734b8f176737fc1d41fd6f6587ea1c
Ayush Choudhary <ayush809044@gmail.com>
2025-12-15 02:25:17 +0530

    created encoder and decoder input

--- dataset.py
+++ dataset.py
@@ -11,3 +11,51 @@ class BilingualDataset(Dataset):
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
         self.seq_len = seq_len
+
+        self.sos_token = torch.tensor([tokenizer_src.token_to_id('[SOS]')],dtype = torch.int64)
+        self.eos_token = torch.tensor([tokenizer_src.token_to_id('[EOS]')],dtype = torch.int64)
+        self.pad_token = torch.tensor([tokenizer_src.token_to_id('[PAD]')],dtype = torch.int64)
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self,index : any) -> any :
+        src_target_pair = self.ds[index]
+        src_text = src_target_pair['translation'][self.src_lang]
+        tgt_text = src_target_pair['translation'][self.tgt_lang]
+
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(src_text).ids
+
+        enc_num_padding_tokens  = self.seq_len - len(enc_input_tokens) - 2
+        dec_num_padding_tokens  = self.seq_len - len(dec_input_tokens) - 1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0 :
+            raise ValueError('Sentence is too long')
+        
+        # Add SOS and EOS to source text 
+
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens,dtype = torch.int64),
+                self.eos_token,
+                torch.tensor(self.pad_token * enc_num_padding_tokens,dtype = torch.int64)
+            ]
+        )
+
+        decoder_input = torch.cat(
+            [
+                self.eos_token,
+                torch.tensor(dec_input_tokens,dtype = torch.int64),
+                torch.tensor(self.pad_token * dec_num_padding_tokens,dtype = torch.int64)
+            ]
+        )
+
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens,dtype = torch.int64),
+                self.eos_token,
+                torch.tensor(self.pad_token * dec_num_padding_tokens,dtype = torch.int64)
+            ]
+        )
